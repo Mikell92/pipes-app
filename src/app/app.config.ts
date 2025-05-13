9a42064445343c8e05b7e8bb91ebb7ad893b6158
Miguel Izquierdo <miguelizquierdo35@gmail.com>
2025-05-12 18:14:45 -0600

    Cambiar locale dinámicamente/idiomas 12.155

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,30 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  LOCALE_ID,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+
+import localeES from '@angular/common/locales/es-MX';
+import localeFr from '@angular/common/locales/fr';
+import { LocalService } from './services/locale.service';
+
+registerLocaleData(localeES, 'es');
+registerLocaleData(localeFr, 'fr');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+
+    {
+      provide: LOCALE_ID,
+      /* useValue: 'es', */
+      deps: [LocalService],
+      useFactory: (localeService: LocalService) => localeService.getLocale,
+    },
+  ],
 };
--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,8 +1,38 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  DatePipe,
+  LowerCasePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
+import { Component, effect, inject, LOCALE_ID, signal } from '@angular/core';
+import { AvailableLocale, LocalService } from '../../services/locale.service';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
 })
-export default class BasicPageComponent {}
+export default class BasicPageComponent {
+  localeService = inject(LocalService);
+  currentLocale = signal(inject(LOCALE_ID));
+
+  nameLower = signal('miguel');
+  nameUpper = signal('MIGUEL');
+  fullName = signal('MigUEL IZquieRdo');
+
+  customDate = signal(new Date());
+
+  tickingDateEffect = effect((onCleanup) => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+    }, 1000);
+
+    onCleanup(() => {
+      clearInterval(interval);
+    });
+  });
+
+  changeLocale(locale: AvailableLocale) {
+    this.localeService.changeLocale(locale);
+  }
+}
